feb56604f343d443290874b545a0ea07375445c5
Chris Sheehan <chris.sheehan@ymail.com>
2016-07-19 23:15:42 +0100

    Initial twitter setup, posts tweet

--- server/main.js
+++ server/main.js
@@ -3,3 +3,20 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+var options = {
+    consumer_key: Meteor.settings.private.twitter.consumer_key,
+    consumer_secret: Meteor.settings.private.twitter.consumer_secret,
+    access_token_key: Meteor.settings.private.twitter.access_token,
+    access_token_secret: Meteor.settings.private.twitter.access_secret
+}
+
+var client = new Twitter(options);
+
+
+Twitter.postAsync(client, 'statuses/update', {status: 'Test for app'},  function(error, tweet, response){
+  console.log(response);  // Raw response object. 
+  if(error) throw error;
+  console.log(tweet);  // Tweet body. 
+  console.log(response);  // Raw response object. 
+});

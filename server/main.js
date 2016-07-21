ded7d096c0287a433b15eccef2cfcb3f2e9b200b
Chris Sheehan <chris.sheehan@ymail.com>
2016-07-21 13:27:33 +0100

    Now  accessing twitter stream  using geolocation

--- server/main.js
+++ server/main.js
@@ -12,11 +12,17 @@ var options = {
 }
 
 var client = new Twitter(options);
+var glasgow_location = '-4.4901557, 55.74857, -3.8721748, 55.9597760';
+var uk_location = '-5.6299873, 50.0481060, -1.9385810, 57.6202252';
 
+Twitter.streamAsync(client, 'statuses/filter', {locations: glasgow_location},
+ function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
 
-Twitter.postAsync(client, 'statuses/update', {status: 'Test for app'},  function(error, tweet, response){
-  console.log(response);  // Raw response object. 
-  if(error) throw error;
-  console.log(tweet);  // Tweet body. 
-  console.log(response);  // Raw response object. 
+  stream.on('error', function(error) {
+    console.log(error);
+    throw error;
+  });
 });

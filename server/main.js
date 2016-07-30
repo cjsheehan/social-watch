7dc50363ee57e1a0fd9565723aede0dcd6d29b67
Chris Sheehan <chris.sheehan@ymail.com>
2016-07-30 17:53:30 +0100

    outputStream refactored to util

--- server/main.js
+++ server/main.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars*/
 import { Meteor } from "meteor/meteor";
+import { formatTweet } from "./twitter/tweet.js";
+import { getTestTweets, populateDb } from "./util";
 /* eslint-enable no-unused-vars*/
 
-import { formatTweet } from "./twitter/tweet.js";
+import "../collections/Tweets.js";
 
 Meteor.startup(() => {
 	// code to run on server at startup
@@ -11,34 +13,12 @@ Meteor.startup(() => {
 // streamTwitter(glasgow_bbox);
 
 
-outputStream();
+// outputStream();
+let tweets = getTestTweets();
+// console.log(tweets);
+populateDb(tweets);
 
-function outputStream() {
-	let jsonTweet = Assets.getText("tweet_stream.json");
-	let tweets = JSON.parse(jsonTweet);
-	let formatAs = "json";
-
-
-	for (let i = 0; i < tweets.length; i++) {
-		let formatted = formatTweet(tweets[i], formatAs);
-		if(formatAs === "json") {
-			try {
-				let fs = Npm.require("fs");
-				console.log("cwd:" + process.cwd());
-				fs.appendFile("../../../../../../../tweet_stream_formatted.json", JSON.stringify(formatted), function (err) {
-					if (err) {
-						return console.log(err);
-					}
-				});
-			} catch (error) {
-				return console.log(error);
-			}
-		} else {
-			console.log("tweet: " + i);
-			console.log(formatted);
-		}
-	}
-}
+// outputStream(tweets, "string");
 
 
 

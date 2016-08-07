5e0bc62ced0ac91aa4c907d0e64e754878182998
Chris Sheehan <chris.sheehan@ymail.com>
2016-08-07 16:50:42 +0100

    format sentiment summary as table

--- client/twitter/Sentiment.js
+++ client/twitter/Sentiment.js
@@ -2,10 +2,24 @@ import { analyze } from "Sentimental";
 
 Template.Sentiment.helpers({
 	sentiment: (text) => {
+		console.log("text:" + text);
+		
 		let sentiment = analyze(text);
-		// console.log(text);
-		// console.log(sentiment.score);
-		console.log(sentiment);
+		console.log(JSON.stringify(sentiment));
 		return sentiment;
+	},
+
+	evalScore: (score, isNegative) => {
+		if (score > 0) {
+			if(isNegative) {
+				return "neg-sentiment";
+			} else {
+				return "pos-sentiment";
+			}
+		} else if(score < 0) {
+			return "neg-sentiment";
+		} else {
+			return "neutral-sentiment";
+		}
 	}
 });

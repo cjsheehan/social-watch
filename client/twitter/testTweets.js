480731b9fdc13127265199eb7110f0ecc2987968
Chris Sheehan <chris.sheehan@ymail.com>
2016-08-12 19:34:58 +0100

    tweet formatting improve formatting of tweet meta data

--- client/twitter/testTweets.js
+++ client/twitter/testTweets.js
@@ -1,702 +1,702 @@
 export const testTweets = [{
-	userName: "test",
+	userName: "I am a User",
 	location: [
 		0.1,
 		0.2
 	],
-	createdAt: "test",
+	createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 	text: "@letshugbro THAT WAS SO GOOD AND I ENJOY FIGHTING YOU JIM."
 },
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@RosesAreBIack @PhatPoch I meant ours vs arsenal."
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@Yseky xDDD \nDesde que soy un guiri no leo bien las cosas xDD\n(Puedo cambiar mi respuesta?)"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "19 degrees is that weird temperature where it's either nice and warm or too warm. Scottish summer is a nightmare"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "ZeeMaH is 13\nIt would appear it is Odin_Voidz now... https://t.co/5VpH3vvjcJ"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "The Pope using Uber in Krakow to get about https://t.co/hUMc3AGdr1"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Get so stressed out going shopping with friends"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "https://t.co/Nq3isSgKti Diversity in Action! @NorthWest_NA @NF_14WORDS @Ironwand @DavidWPWW @Freyja_Griffith @kate_mabers @imogen19091"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@realDonaldTrump @robdelaney FUCKSAKES!!! NUTCASE!"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "A selfie in the square.  #georgesquare #scotland #glasgow #wedding @ George Square https://t.co/taEK8QuahB",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@SentientRoomba Thanks! I had a lot of fun!",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Drinking an ipa (belma, azacca, citra, el dorado) by @gallus_brewing @ Linn Park — https://t.co/3aSukY1HLG #photo",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Ryannnnn, @ryanpollock_ is now trending in #Glasgow https://t.co/BEMuEimC8v",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@Rubiks_Official Lovely giveaway, thank you ,rt'd and followed . #prizedraw",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@RyzenT7 @OdinKeza shame on you",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@radicalshivv lmfaoo ledge FTP !!",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Battling with injury to be on the start line with @ONEProCycling for #RideLondon on Sunday.  Thanks @run_pepper for your efforts"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@Biggie_Sauls haha his aunt picked him those up from the US last month. He has a better collection than i do!",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "can i request \n\nJonas Blue ft JP Cooper - Perfect Strangers \n\nPlease Mr King \n\n@MeridianRadio",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "un carnet un cahier à spirale et un genre de truc pour faire des to do lists",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "How do we just move on?",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "dans lesquels j'écrirais jamais",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@xahsinom omfg warehouse prject followed u!!!!! amazing",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Did a quick para-check that the PS4 Camera wasn't on there because I'm now sprawled out on the sofa like a Lothario.",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "i 2 show uall my love\n\nowu liqe t bbs\ni gotu\njus writnmysqin\nwith a blade so thin\n\n#pandafanta https://t.co/niZWdSy9Ci",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@OdinKeza The same as your gold count",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Would you risk it for a chocolate biscuit?",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@RosesAreBIack @PhatPoch different point but that would be bless hate that twat",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "The sun was reason enough to come down. ߌԠ#tapsaff #payweekend #bankholiday @ Saint Luke's https://t.co/npNGNoTIEE",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "BELGIANS LOVE HORSE MEAT. THE 'BRAINS' OF THE EU (IF YOU CAN CALL A FUCKUP THAT) IS IN THE HAGUE. GET THE PICTURE? https://t.co/dQnXj1k8Pt",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@BitSocket this is the Dark Souls of tweets.",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "(Lothario is my favourite Pokémon)\n#TopicalPatter",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Introduced my maw to snapchat hahahahaha https://t.co/SQgFPteJYU",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@garyogmusic st Vincent's or this new bar? Was planning a trip to hope st the night?",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@ArtyBagger @gwlkettle We adore you too dearest Muriel &amp; appreciate everything you have done to support &amp; encourage us x",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@shuggie116 and proud to speak for the.majority",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@Rubiks_Official https://t.co/8kMdFw6A0Y",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@OdinKeza The same as your dad count",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Why do nandos wait till you've ordered and they've taken your money to tell you it will 30 mins for food!!!!!!!!",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Look forward to buying stuff using Apple Pay https://t.co/K3AecVQQOd",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Lads. Here; lads. And ladies. Here. Listen. Listen to this: Street Fighter is very good.",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@PaulRamsay7 @jimwilsonhome1 @keithfelly get doon early  for tache fitting and team photo #smile",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "BELGIANS LOVE HORSE MEAT. THE 'BRAINS' OF THE EU (IF YOU CAN CALL A FUCKUP THAT) IS IN THE HAGUE. GET THE PICTURE? https://t.co/CO9HjkB4cJ",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@OdinKeza HAHAHA",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Merlins birthday cake with Markies icing @ Hopefield Gardens https://t.co/MAUvbv3IFB",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "I get v emotional when I'm hungry and hungover",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@detective_truth look what made it north! I call it “The Simon Quartet” https://t.co/q9JbCCh5zS",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@kidd_kong78 WTF?!",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@akapaulineblack @official_isme thanks for coming!",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@RosesAreBIack @EASPORTSFIFA fuck me that's going to make me play  it again",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@JohnnyDundee @DouglasDaniel Maddox is writing fiction and it should be sold as that.",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@dr_dyb @DrScottie79 I've been left at home on my own. Considering chicken wings and chips.",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@UlstersNo1 @jordan_rossiter I wonder if he will play in one this year, or later?? \ntotally agree though..",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Beautiful day with iainmmcgregor and the kids out at Yellowcraigs Beach. Sun, sea and many… https://t.co/Swuqn4hP5D",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@garngadpat51 @BoboRollcage @celticfc ok mate......I've passed it on"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Oi you ya cunt get tae fuck ya wee arse licking bully  https://t.co/SK96EwohOt",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@Jessmarwoodx @eden_tdfan just don't respond then she will get bored of you and Louise is on",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "No sé qué hacéis respirando, cuando podríais estar defendiendo vuestros derechos, o luchando por una sanidad digna.",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@LadyCatHT so far the allotment has delivered baked potatoes and rhubarb gin. That's what they're for, right?",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Birthday cake for all! @ Hopefield Gardens https://t.co/9vShSx4BEG",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@grummmusic you fucking beauty! ߎɰߎɰߎɠ#ABGT200",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "It's the weekend"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "LET I M C R Y ING WHEN WILL I MARRY ME AND GIVE ME THAT LIWNE PM E A SE",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Now now you know you love it Jess  https://t.co/L1sRA72M8b",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@georgiejones134 Chloe's trying to ruin my life with that gif @eden_tdfan",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "LRT I M C R Y ING WHEN WILL I MARRY ME AND GIVE ME THAT LIWNE PM E A SE",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@SaeviorLS No sé qué hacéis durmiendo, cuando podríais estar luchando por el hambre en el mundo. Y por la paz.",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "my figure is an absolute HATE rnߙðߘհߘ͡!",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@aberdeenmag shocking not enough markers for pitchfork ...#realclubdoes"
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@shelly_coyne @DiljeetB_Flute likewise! Soon.",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "That's me back Scotland-side.",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@Ivan_McKee @AnneMcLaughlin hope my e mail finds you both well and together with the Society of William Wallace can get this resolved",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "LRT I M C R Y ING WHEN WILL HE MARRY ME AND GIVE ME THAT LIWNE PM E A SE",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Ayew staying then ߘà20+ goals this season,even Gestede scored 15+ in the championship aswell,still want Mccormack @AVFCOfficial @Dr_TonyXia",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "Happy birthday @rosie_bentham , hope you have a great day sweetheart :-D x x x! &lt;3 ♥♡ ߎɰߎpߎȰߎ :-D",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@xahsinom it tht feed man",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@scottishsteveo it's sunny here",
 	},
 	{
-		userName: "test",
+		userName: "I am a User",
 		location: [
 			0.1,
 			0.2
 		],
-		createdAt: "test",
+		createdAt: "Fri Jul 29 18:22:32 +0000 2016",
 		text: "@nuthall1862 @marblebrewers An excellent beer.",
 	}];

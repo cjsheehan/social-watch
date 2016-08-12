446327996307e817d61e15351f72b50a99f0059e
Chris Sheehan <chris.sheehan@ymail.com>
2016-08-12 19:33:10 +0100

    Convert twitter created_at field to suitable date

---
+++ client/twitter/Tweet.js
@@ -0,0 +1,8 @@
+import moment from "moment"; 
+
+Template.Tweet.helpers({
+	dateTime: (createdAt) => {
+		// e.g. "Fri Jul 29 18:22:32 +0000 2016"
+		return moment(createdAt, "dd MMM DD HH:mm:ss ZZ YYYY", "en").format("MMM DD, HH:mm");
+	}
+});

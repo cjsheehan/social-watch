3caafadc6ef89b4aff0179a151941864ae8e718d
Chris Sheehan <chris.sheehan@ymail.com>
2016-08-14 14:25:19 +0100

    basic chart imporved to use complex data set and svg

--- client/Chart.js
+++ client/Chart.js
@@ -1,28 +1,41 @@
 const d3 = require("d3");
 
-// let data = [
-// 	{ name: "Locke", value: 4 },
-// 	{ name: "Reyes", value: 8 },
-// 	{ name: "Ford", value: 15 },
-// 	{ name: "Jarrah", value: 16 },
-// 	{ name: "Shephard", value: 23 },
-// 	{ name: "Kwon", value: 42 }
-// ];
-
-
 Template.Chart.onRendered(function () {
 
-	var data = [4, 8, 15, 16, 23, 42];
+	let data = [
+		{ name: "Locke", value: 4 },
+		{ name: "Reyes", value: 8 },
+		{ name: "Ford", value: 15 },
+		{ name: "Jarrah", value: 16 },
+		{ name: "Shephard", value: 23 },
+		{ name: "Kwon", value: 18 }
+	];
+
+	var width = 200,
+		barHeight = 20;
+
 
 	var x = d3.scaleLinear()
-		.domain([0, d3.max(data)])
-		.range([0, 420]);
+		.domain([0, d3.max(data.map(function(d){ return d.value;} ))])
+		.range([0, width]);
 
-	d3.select(".chart")
-		.selectAll("div")
-		.data(data)
-		.enter().append("div")
-		.style("width", function (d) { return x(d) + "px"; })
-		.text(function (d) { return d; });
+	var chart = d3.select(".chart")
+		.attr("width", width);
 
+		chart.attr("height", barHeight * data.length);
+
+		var bar = chart.selectAll("g")
+			.data(data)
+			.enter().append("g")
+			.attr("transform", function (d, i) { return "translate(0," + i * barHeight + ")"; });
+
+		bar.append("rect")
+			.attr("width", function (d) { return x(d.value); })
+			.attr("height", barHeight - 1);
+
+		bar.append("text")
+			.attr("x", function (d) { return x(d.value) - 3; })
+			.attr("y", barHeight / 2)
+			.attr("dy", ".35em")
+			.text(function (d) { return d.value; });
 });

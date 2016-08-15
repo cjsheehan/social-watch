020379a5207c162d1a4c310a7ad07cb3fbe79a0f
Chris Sheehan <chris.sheehan@ymail.com>
2016-08-16 00:15:35 +0100

    refactored to use common sentiment helper
    sentiment helper creates sorted table data for pos/neg core/comparative

--- client/SentimentResults.js
+++ client/SentimentResults.js
@@ -1,37 +1,54 @@
-import { SentimentResults } from "./twitter/Tweets";
+import { MAX_SENTIMENT_ROWS } from "./lib/constants";
 const Highcharts = require("highcharts/highstock");
 
-Template.SentimentResults.onCreated(function () {
-	var self = this;
-	self.autorun(function () {
-		self.subscribe("results");
-	});
-	this.count = 0;
-});
-
 Template.SentimentResults.helpers({
-	frequency: () => {
+
+	sentiment: (attribute, isPositive = true) => {
+
+		// wordStats[attribute] is sorted descending list wrt attribute
 		let wordStats = Session.get("activeWordStats");
 		let results = [];
-		let frequencySeries = [];
-
-		if (wordStats != null && wordStats.hasOwnProperty("frequency")) {
-			for (let i = 0; i < 10; i++) {
-				if (wordStats.score[i] != null) {
-					let word = wordStats.frequency[i][0];
-					let res = {
-						"word": word,
-						"stats": wordStats.words[word]
+		
+		if (wordStats != null && wordStats.hasOwnProperty([attribute])) {
+			if (isPositive) {
+				// iterate forward in array
+				for (let i = 0; i < MAX_SENTIMENT_ROWS && i < wordStats[attribute].length; i++) {
+					if (wordStats.score[i] != null) {
+						let word = wordStats[attribute][i][0];
+						let res = {
+							"word": word,
+							"stats": wordStats.words[word]
+						}
+						results.push(res);
 					}
-
-					results.push(res);
+				}
+			} else {
+				// iterate backwards in array
+				let start = wordStats[attribute].length - 1;
+				let end = start - MAX_SENTIMENT_ROWS;
+				for (let i = start; i > end && i >= 0; i--) {
+					if (wordStats[attribute][i] != null) {
+						let word = wordStats[attribute][i][0];
+						let res = {
+							"word": word,
+							"stats": wordStats.words[word]
+						}
+						results.push(res);
+					}
 				}
 			}
 		}
 
-		// console.log("results", JSON.stringify(results));
-		Session.set("activeFreqStats", results);
 		return results;
+	},
+
+	percentOfTweets: (word) => {
+		let wordStats = Session.get("activeWordStats");
+		let percent = 0;
+		if (wordStats != null && wordStats.words[word] != null) {
+			percent = 100 * (wordStats.words[word].frequency / wordStats.numTweets);
+		}
+		return percent;
 	},
 
 	frequencyChart: () => {
@@ -114,100 +131,5 @@ Template.SentimentResults.helpers({
 			});
 		});
 	},
-
-	scorePos: () => {
-		let wordStats = Session.get("activeWordStats");
-		let results = [];
-
-		if (wordStats != null && wordStats.hasOwnProperty("score")) {
-			for (let i = 0; i < 5 && i < wordStats.score.length; i++) {
-				if (wordStats.score[i] != null) {
-					let word = wordStats.score[i][0];
-					let res = {
-						"word": word,
-						"stats": wordStats.words[word]
-					}
-					results.push(res);
-				}
-			}
-		}
-
-		Session.set("activePosStats", results);
-		return results;
-	},
-
-	scoreNeg: () => {
-		let wordStats = Session.get("activeWordStats");
-		let results = [];
-
-		if (wordStats != null && wordStats.hasOwnProperty("score")) {
-			let start = wordStats.score.length - 1;
-			let end = start - 5;
-			for (let i = start; i >= end; i--) {
-				if (wordStats.score[i] != null) {
-					let word = wordStats.score[i][0];
-					let res = {
-						"word": word,
-						"stats": wordStats.words[word]
-					}
-					results.push(res);
-				}
-			}
-		}
-		Session.set("activeNegStats", results);
-		return results;
-	},
-
-	percentOfTweets: (word) => {
-		let wordStats = Session.get("activeWordStats");
-		let percent = 0;
-		if (wordStats != null && wordStats.words[word] != null) {
-			percent = 100 * (wordStats.words[word].frequency / wordStats.numTweets);
-		}
-		return percent;
-	}
 });
 
-function populateResultsDb(results) {
-	// console.log("Results: " + JSON.stringify(results));
-	results.forEach(function (result) {
-		SentimentResults.insert(result);
-	});
-}
-
-const testResults = [
-	{
-		"carrrrr": {
-			sentiment: {
-				score: 6,
-				comparative: 1.25,
-				positive: {
-					score: 8,
-					comparative: 1.25,
-				},
-				negative: {
-					score: -3,
-					comparative: 1.25,
-				}
-			}
-		},
-
-	},
-	{
-		"toy": {
-			sentiment: {
-				score: 6,
-				comparative: 1.25,
-				positive: {
-					score: 8,
-					comparative: 1.25,
-				},
-				negative: {
-					score: -3,
-					comparative: 1.25,
-				}
-			}
-		},
-
-	},
-];
---
+++ client/lib/constants.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-unused-vars*/
+export const MAX_SENTIMENT_ROWS = 5;
+/* eslint-enable no-unused-vars*/

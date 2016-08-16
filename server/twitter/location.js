753e87237ef65bc8934ba31ba6d1bbff621a4d6a
Chris Sheehan <chris.sheehan@ymail.com>
2016-08-16 15:58:58 +0100

    twitter stream insert to db streamAsync callback now implements calls to format tweet and insert to collection

--- server/twitter/location.js
+++ server/twitter/location.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-unused-vars*/
-const glasgow_bbox = [-4.4901557, 55.74857, -3.8721748, 55.9597760];
-const uk_bbox = [-5.6299873, 50.0481060, -1.9385810, 57.6202252];
+export const glasgow_bbox = [-4.4901557, 55.74857, -3.8721748, 55.9597760];
+export const uk_bbox = [-5.6299873, 50.0481060, -1.9385810, 57.6202252];
 /* eslint-enable no-unused-vars*/
--- server/twitter/twitter.js
+++ server/twitter/twitter.js
@@ -1,4 +1,7 @@
 import { ArgumentException } from "/lib/exceptions";
+import { formatTweet } from "./tweet";
+import { insertTweet } from "./db";
+import { Tweets } from "/collections/Tweets";
 
 const options = {
 	consumer_key: Meteor.settings.private.twitter.consumer_key,
@@ -7,33 +10,27 @@ const options = {
 	access_token_secret: Meteor.settings.private.twitter.access_secret
 }
 
-let fs = Npm.require("fs");
+export function streamTwitter(loc) {
 
-export function streamTwitter(locations) {
-
-	if (locations == null) {
+	if (loc == null) {
 		throw new ArgumentException("locations argument must be provided");
-	} else if (locations.constructor !== Array) {
+	} else if (loc.constructor !== Array) {
 		throw new ArgumentException("locations argument must be an array of numbers");
 	} 
 
 	try {
 		let client = new Twitter(options);
-		Twitter.streamAsync(client, "statuses/filter", { LOCATIONS_KEY: locations.toString() },
+		Twitter.streamAsync(client, "statuses/filter", { locations: loc.toString() },
 			function (stream) {
-
-				stream.on("data", function (tweet) {
-					console.log(tweet.text);
+				stream.on("data", Meteor.bindEnvironment(function (tweet) {
 					try {
-						fs.appendFile("../../../../../../../tweets.txt", JSON.stringify(tweet), function (err) {
-							if (err) {
-								return console.log(err);
-							}
-						});
+						let formatted = formatTweet(tweet, "db");
+						console.log("formatted", JSON.stringify(formatted));
+						insertTweet(formatted, Tweets);
 					} catch (error) {
-						return console.log(error);
+						console.log(error);
 					}
-				});
+				}));
 
 				stream.on("error", function (error) {
 					throw error;
@@ -42,6 +39,6 @@ export function streamTwitter(locations) {
 			});
 
 	} catch (error) {
-		console.log(error);
+		console.log("Twitter.streamAsync error: ", error);
 	}
 }
